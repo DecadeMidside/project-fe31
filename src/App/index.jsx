a6f27bae7d60ca2df9a1573b489e1e4eba2b2841
PhanThanhVinh <phanthanhvinh22121996@gmail.com>
2023-04-12 19:29:36 +0700

    fix

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,20 +1,12 @@
 import { ConfigProvider } from "antd";
 import Header from "../layout/Header";
 import Footer from "../layout/Footer";
-// import  from "";
 import ProductList from "../pages/ProductList";
 
 function App() {
   return (
     <div className="App">
-      <ConfigProvider
-      // theme={{
-      //   token: {
-      //     colorPrimary: "",
-      //     colorText: "black",
-      //   },
-      // }}
-      >
+      <ConfigProvider>
         <Header />
         <Footer />
         <ProductList />
